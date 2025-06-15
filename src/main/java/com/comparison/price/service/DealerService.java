3d5441ea7786c9119e7c01b5ebc87653ff2fbb07
Muzaffar Qosimov <qwertymuzaffar@gmail.com>
2025-06-15 16:33:03 -0400

    Read products and dealers from JSON

--- src/main/java/com/comparison/price/service/DealerService.java
+++ src/main/java/com/comparison/price/service/DealerService.java
@@ -42,17 +42,12 @@ public class DealerService {
     }
 
     private DealersDTO getData() {
-        var dealers = new DealersDTO();
-        List<DealerDTO> dealersList = Arrays.asList(
-            new DealerDTO("Binglee", Map.of("Headphones", "$30", "Printer", "$75")),
-            new DealerDTO("DXC Electronics", Map.of("Mouse", "$20", "Printer", "$85", "Headphones", "$20")),
-            new DealerDTO("Bobay", Map.of("Headphones", "$20", "Printer", "$80")),
-            new DealerDTO("Tech City", Map.of("Mouse", "$20", "Laptop", "$850")),
-            new DealerDTO("Ez PC", Map.of("Laptop", "$1000")),
-            new DealerDTO("GH Computers", Map.of("Laptop", "$1500", "Printer", "$95"))
-        );
-        dealers.setDealers(dealersList);
-        return dealers;
+        try {
+            String json = JsonReader.readJsonFromClasspath("json/dealers.json");
+            return objectMapper.readValue(json, DealersDTO.class);
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     public List<AbstractMap.SimpleEntry<String, String>> getAllProductPrices(String productName) {
